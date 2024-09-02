d61a2ed40c2dadcc1ede1042e849e069e62f7b13
FrankieMarie <frankiemarie83@gmail.com>
2024-09-02 12:55:54 -0500

    handle drag events for carousel

--- js/carousel.js
+++ js/carousel.js
@@ -32,10 +32,52 @@ function hideShowArrows(slides, targetIndex, prevBtn, nextBtn) {
   }
 }
 
-tracks.forEach((track) => {
+tracks.forEach((track, i) => {
+  // set slide positions
   Array.from(track.children).forEach(setSlidePosition);
+
+  // handle drag events for slides
+  let initialX = 0;
+  let dragged = false;
+
+  track.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    initialX = e.clientX;
+    dragged = false;
+  });
+
+  track.addEventListener('mousemove', (e) => {
+    dragged = true;
+  });
+
+  track.addEventListener('mouseup', (e) => {
+    const slides = Array.from(track.children);
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    const prevIndex = slides.findIndex((slide) => slide === prevSlide);
+    const nextSlide = currentSlide.nextElementSibling;
+    const nextIndex = slides.findIndex((slide) => slide === nextSlide);
+    const currentDot = dotsNavs[i].querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling;
+    const prevDot = currentDot.previousElementSibling;
+    const prevBtn = prevBtns[i];
+    const nextBtn = nextBtns[i];
+
+    if (dragged && e.clientX < initialX) {
+      // drag left, move next
+      moveToSlide(track, currentSlide, nextSlide);
+      updateDots(currentDot, nextDot);
+      hideShowArrows(slides, nextIndex, prevBtn, nextBtn);
+    } else if (dragged && e.clientX > initialX) {
+      // drag right, move prev
+      moveToSlide(track, currentSlide, prevSlide);
+      updateDots(currentDot, prevDot);
+      hideShowArrows(slides, prevIndex, prevBtn, nextBtn);
+    }
+  });
 });
 
+// update slide and buttons on next click
 nextBtns.forEach((nextBtn, i) => {
   const track = tracks[i];
   const dotsNav = dotsNavs[i];
@@ -55,6 +97,7 @@ nextBtns.forEach((nextBtn, i) => {
   });
 });
 
+// update slide and buttons on prev click
 prevBtns.forEach((prevBtn, i) => {
   const track = tracks[i];
   const dotsNav = dotsNavs[i];
@@ -74,6 +117,7 @@ prevBtns.forEach((prevBtn, i) => {
   });
 });
 
+// update slide and buttons on dot click
 dotsNavs.forEach((dotsNav, i) => {
   const track = tracks[i];
   const slides = Array.from(track.children);
